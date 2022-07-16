5f1debfae7b9878abb684e49b50e51e79fd6617d
imstuff <bayoentm8@gmail.com>
2022-07-16 13:30:18 -0400

    Initial Commit

---
+++ app/src/main/java/edu/neu/madcourse/team20_finalproject/MainActivity.java
@@ -0,0 +1,17 @@
+package edu.neu.madcourse.team20_finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ *
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
