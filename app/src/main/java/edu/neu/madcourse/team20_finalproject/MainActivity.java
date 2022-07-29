369dc7ec593a3da9d58e6297c62bf507a7932d7b
Yijing Zheng <zheng.yij@northeastern.edu>
2022-07-29 15:26:49 -0700

    added login days

--- app/src/main/java/edu/neu/madcourse/team20_finalproject/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/team20_finalproject/MainActivity.java
@@ -6,6 +6,10 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import edu.neu.madcourse.team20_finalproject.perfomance.Sound;
 import edu.neu.madcourse.team20_finalproject.perfomance.Vibration;
@@ -18,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     private static final String MUSIC = "music";
     private static final String SOUND_EFFECT = "soundEffect";
     private static final String VIBRATION = "vibration";
+    private static final String LOG_ON_DAYS = "logOnDays";
+    private static final String LAST_LOGIN = "lastLogin";
 
     private Sound bgm;
     private Sound se;
@@ -27,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
     private boolean muteBgm;
     private boolean muteSe;
     private boolean stopVb;
+    private int logOnDays;
+    private String lastLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,11 +72,33 @@ public class MainActivity extends AppCompatActivity {
         muteBgm = sharedPreferences.getBoolean(MUSIC, false);
         muteSe = sharedPreferences.getBoolean(SOUND_EFFECT, false);
         stopVb = sharedPreferences.getBoolean(VIBRATION, false);
+        logOnDays = sharedPreferences.getInt(LOG_ON_DAYS, 0);
+        lastLogin = sharedPreferences.getString(LAST_LOGIN, "");
+    }
+
+    private void saveLoginDays() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SETTINGS,MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(LOG_ON_DAYS, logOnDays);
+        editor.putString(LAST_LOGIN, lastLogin);
+        editor.apply();
+    }
+
+    private void updateLoginDays() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar calendar = Calendar.getInstance();
+        String date = sdf.format(calendar.getTime());
+        if (!lastLogin.equals(date)) {
+            lastLogin = date;
+            logOnDays += 1;
+            Toast.makeText(this, "First Login Today!", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        saveLoginDays();
         bgm.stopSound();
     }
 
@@ -76,6 +106,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         loadData();
+        updateLoginDays();
         bgm.playSound(muteBgm, this, R.raw.menu, true);
     }
 }
